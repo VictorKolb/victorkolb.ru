20617666904aba9507f263590c16085d77642871
victorkolb <violetmonox@gmail.com>
2018-04-25 17:18:25 +0300

    а давай попроще

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = mongoose.Schema({
-  title: {type: String, required: true},
-  body: {type: String, required: true},
-  posted: {type: Date, default: new Date}
-}, {collection: 'post'});
-
-module.exports = mongoose.model('PostModel', PostSchema);
